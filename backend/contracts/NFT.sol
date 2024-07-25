96b1785181789f1a39c6e16f4acec6fd4f0d25d8
Ken <rainyyj47@gmail.com>
2024-07-25 18:09:09 +0800

    code update

--- backend/contracts/NFT.sol
+++ backend/contracts/NFT.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -7,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract CoreNFT is ERC721, ERC721URIStorage, Ownable {
+contract CoreNFT is ERC721, ERC721URIStorage, Ownable { 
     using Strings for uint256;
 
     uint256 private _nextTokenId;
@@ -53,5 +52,5 @@ contract CoreNFT is ERC721, ERC721URIStorage, Ownable {
     {
         return super.supportsInterface(interfaceId);
     }
-
 }
+
